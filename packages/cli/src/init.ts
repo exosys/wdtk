7e56e5ad20aa20f8a8bb303124e4ea95e8f3f0c2
bogdant <bogdant@ganz.com>
2018-12-11 16:27:02 -0500

    More implementation

--- packages/cli/src/init.ts
+++ packages/cli/src/init.ts
@@ -1,4 +1,11 @@
-// import 'symbol-observable';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import 'symbol-observable';
 console.error('hello from' + process.title);
 
 let cli;
