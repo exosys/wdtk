7f21b54b41ae8d5e24306662558ea02a2c77c789
bogdant <bogdant@ganz.com>
2018-12-17 12:33:34 -0500

    Preparations to run jest unit tests

--- packages/schematics/src/collection/ng-app/index.spec.ts
+++ packages/schematics/src/collection/ng-app/index.spec.ts
@@ -1,7 +1,31 @@
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
+import { Schema as ApplicationOptions } from '@schematics/angular/application/schema';
 
 describe('Angular Application Schematic', () => {
+  const schematicRunner = new SchematicTestRunner('@wdtk/schematics', require.resolve('../../../collection.json'));
+
+  const workspaceOpts: WorkspaceOptions = {
+    name: 'work',
+    newProjectRoot: 'projects',
+    version: '7.0.0'
+  };
+
+  const applicationOpts: ApplicationOptions = {
+    name: 'foo',
+    inlineStyle: false,
+    inlineTemplate: false,
+    routing: false,
+    skipPackageJson: false
+  };
+
   it('should create all files of an application', () => {
+    let workspaceTree: UnitTestTree;
+    workspaceTree = schematicRunner.runExternalSchematic('@schematics/angular', 'workspace', workspaceOpts);
+
     const options = {};
+
+    const tree = schematicRunner.runSchematic('ng-app', options);
   });
+  it('should not do something', () => {});
 });
