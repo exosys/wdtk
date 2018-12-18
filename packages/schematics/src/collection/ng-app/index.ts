1b788412fdea905bbb509425bdebf743402930c2
bogdant <bogdant@ganz.com>
2018-12-18 12:41:16 -0500

    attempt to independently release packages

--- packages/schematics/src/collection/ng-app/index.ts
+++ packages/schematics/src/collection/ng-app/index.ts
@@ -1,10 +1,9 @@
-import { join,normalize } from '@angular-devkit/core';
+import { join, normalize } from '@angular-devkit/core';
 import { Rule, Tree, chain, externalSchematic, move, SchematicsException } from '@angular-devkit/schematics';
 import { getWorkspace, updateWorkspace } from '@schematics/angular/utility/config';
-import {getProject} from "@schematics/angular/utility/project";
+import { getProject } from '@schematics/angular/utility/project';
 import { Schema as Options } from './schema';
 import { toFileName } from './../../util/string';
-
 
 interface NormalizedOptions extends Options {
   appProjectRoot: string;
@@ -14,6 +13,7 @@ interface NormalizedOptions extends Options {
 }
 
 export default function(opts: Options): Rule {
+  //just a comment
   return (tree: Tree) => {
     console.error('here');
     opts = normalizeOptions(tree, opts);
@@ -35,95 +35,85 @@ export default function(opts: Options): Rule {
 }
 
 function updateAppProject(opts: NormalizedOptions): Rule {
-  return (tree: Tree) => {  
+  return (tree: Tree) => {
     const workspace = getWorkspace(tree);
-    let project = getProject(workspace,opts.name);
+    let project = getProject(workspace, opts.name);
 
-    if(!project) {
+    if (!project) {
       throw new SchematicsException(`Project '${opts.name}' does not exist.`);
     }
     const projectRoot = opts.appProjectRoot;
     project.root = projectRoot;
     project.sourceRoot = join(normalize(projectRoot), 'src');
 
-    if(project.architect!==undefined) {
-      if(project.architect.build) {
+    if (project.architect !== undefined) {
+      if (project.architect.build) {
         project.architect.build.options.index = join(normalize(project.sourceRoot), 'index.html');
         project.architect.build.options.main = join(normalize(project.sourceRoot), 'main.ts');
         project.architect.build.options.polyfills = join(normalize(project.sourceRoot), 'polyfills.ts');
-        project.architect.build.options.tsConfig= join(normalize(projectRoot), 'tsconfig.app.json');
-        project.architect.build.options.assets= [
-          join(normalize(project.sourceRoot),'favicon.ico'),
-          join(normalize(project.sourceRoot),'assets')
+        project.architect.build.options.tsConfig = join(normalize(projectRoot), 'tsconfig.app.json');
+        project.architect.build.options.assets = [
+          join(normalize(project.sourceRoot), 'favicon.ico'),
+          join(normalize(project.sourceRoot), 'assets')
         ];
-        project.architect.build.options.styles= [
-          join(normalize(project.sourceRoot),`styles.${opts.style}`)
-        ];
+        project.architect.build.options.styles = [join(normalize(project.sourceRoot), `styles.${opts.style}`)];
 
-        if(project.architect.build.configurations) {
+        if (project.architect.build.configurations) {
           project.architect.build.configurations.production.fileReplacements = [
             {
-              "replace":join(normalize(project.sourceRoot), 'environments','environment.ts'),
-              "with":join(normalize(project.sourceRoot), 'environments','environment.prod.ts'),
+              replace: join(normalize(project.sourceRoot), 'environments', 'environment.ts'),
+              with: join(normalize(project.sourceRoot), 'environments', 'environment.prod.ts')
             }
-          ]
+          ];
         }
       }
 
-      if(project.architect.test) {
+      if (project.architect.test) {
         project.architect.test.options.main = join(normalize(project.sourceRoot), 'test.ts');
         project.architect.test.options.polyfills = join(normalize(project.sourceRoot), 'polyfills.ts');
-        project.architect.test.options.tsConfig= join(normalize(projectRoot), 'tsconfig.spec.json');
+        project.architect.test.options.tsConfig = join(normalize(projectRoot), 'tsconfig.spec.json');
         project.architect.test.options.karmaConfig = join(normalize(projectRoot), 'karma.conf.js');
-        project.architect.test.options.assets= [
-          join(normalize(project.sourceRoot),'favicon.ico'),
-          join(normalize(project.sourceRoot),'assets')
+        project.architect.test.options.assets = [
+          join(normalize(project.sourceRoot), 'favicon.ico'),
+          join(normalize(project.sourceRoot), 'assets')
         ];
-        project.architect.test.options.styles = [
-         
-          join(normalize(project.sourceRoot),`styles.${opts.style}`)
-        ];
+        project.architect.test.options.styles = [join(normalize(project.sourceRoot), `styles.${opts.style}`)];
       }
 
-      if(project.architect.lint) {
+      if (project.architect.lint) {
         project.architect.lint.options.tsConfig = [
           join(normalize(projectRoot), 'tsconfig.app.json'),
           join(normalize(projectRoot), 'tsconfig.spec.json')
-        ]
+        ];
       }
     }
     workspace.projects[opts.name] = project;
     return updateWorkspace(workspace);
-    
   };
 }
 
 function updateE2eProject(opts: NormalizedOptions): Rule {
-  return (tree: Tree) => {  
+  return (tree: Tree) => {
     const workspace = getWorkspace(tree);
-    let project = getProject(workspace,opts.e2eProjectName);
+    let project = getProject(workspace, opts.e2eProjectName);
 
-    if(!project) {
+    if (!project) {
       throw new SchematicsException(`Project '${opts.name}' does not exist.`);
     }
     const projectRoot = opts.e2eProjectRoot;
     project.root = projectRoot;
     project.sourceRoot = join(normalize(projectRoot), 'src');
 
-     if(project.architect!==undefined) {
-      if(project.architect.e2e) {
+    if (project.architect !== undefined) {
+      if (project.architect.e2e) {
         project.architect.e2e.options.protractorConfig = join(normalize(projectRoot), 'protractor.conf.js');
-       
       }
-      if(project.architect.lint) {
-        project.architect.lint.options.tsConfig = [
-          join(normalize(projectRoot), 'tsconfig.e2e.json')
-        ]
+      if (project.architect.lint) {
+        project.architect.lint.options.tsConfig = [join(normalize(projectRoot), 'tsconfig.e2e.json')];
       }
     }
     workspace.projects[opts.e2eProjectName] = project;
     return updateWorkspace(workspace);
-    
   };
 }
 
