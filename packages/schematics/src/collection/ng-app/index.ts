cf49810153ca687d601c1a615ae027bc7a86a987
bogdant <bogdant@ganz.com>
2018-12-18 16:46:45 -0500

    Added updateJson rule

--- packages/schematics/src/collection/ng-app/index.ts
+++ packages/schematics/src/collection/ng-app/index.ts
@@ -4,6 +4,7 @@ import { getWorkspace, updateWorkspace } from '@schematics/angular/utility/confi
 import { getProject } from '@schematics/angular/utility/project';
 import { Schema as Options } from './schema';
 import { toFileName } from './../../util/string';
+import { noop } from 'rxjs';
 
 interface NormalizedOptions extends Options {
   appProjectRoot: string;
@@ -13,9 +14,7 @@ interface NormalizedOptions extends Options {
 }
 
 export default function(opts: Options): Rule {
-  
   return (tree: Tree) => {
-    console.error('here');
     opts = normalizeOptions(tree, opts);
 
     const workspace = getWorkspace(tree);
@@ -35,6 +34,67 @@ export default function(opts: Options): Rule {
 }
 
 function updateAppProject(opts: NormalizedOptions): Rule {
+  return chain([
+    noop,
+    (tree: Tree) => {
+      const workspace = getWorkspace(tree);
+      let project = getProject(workspace, opts.name);
+
+      if (!project) {
+        throw new SchematicsException(`Project '${opts.name}' does not exist.`);
+      }
+      const projectRoot = opts.appProjectRoot;
+      project.root = projectRoot;
+      project.sourceRoot = join(normalize(projectRoot), 'src');
+
+      if (project.architect !== undefined) {
+        if (project.architect.build) {
+          project.architect.build.options.index = join(normalize(project.sourceRoot), 'index.html');
+          project.architect.build.options.main = join(normalize(project.sourceRoot), 'main.ts');
+          project.architect.build.options.polyfills = join(normalize(project.sourceRoot), 'polyfills.ts');
+          project.architect.build.options.tsConfig = join(normalize(projectRoot), 'tsconfig.app.json');
+          project.architect.build.options.assets = [
+            join(normalize(project.sourceRoot), 'favicon.ico'),
+            join(normalize(project.sourceRoot), 'assets')
+          ];
+          project.architect.build.options.styles = [join(normalize(project.sourceRoot), `styles.${opts.style}`)];
+
+          if (project.architect.build.configurations) {
+            project.architect.build.configurations.production.fileReplacements = [
+              {
+                replace: join(normalize(project.sourceRoot), 'environments', 'environment.ts'),
+                with: join(normalize(project.sourceRoot), 'environments', 'environment.prod.ts')
+              }
+            ];
+          }
+        }
+
+        if (project.architect.test) {
+          project.architect.test.options.main = join(normalize(project.sourceRoot), 'test.ts');
+          project.architect.test.options.polyfills = join(normalize(project.sourceRoot), 'polyfills.ts');
+          project.architect.test.options.tsConfig = join(normalize(projectRoot), 'tsconfig.spec.json');
+          project.architect.test.options.karmaConfig = join(normalize(projectRoot), 'karma.conf.js');
+          project.architect.test.options.assets = [
+            join(normalize(project.sourceRoot), 'favicon.ico'),
+            join(normalize(project.sourceRoot), 'assets')
+          ];
+          project.architect.test.options.styles = [join(normalize(project.sourceRoot), `styles.${opts.style}`)];
+        }
+
+        if (project.architect.lint) {
+          project.architect.lint.options.tsConfig = [
+            join(normalize(projectRoot), 'tsconfig.app.json'),
+            join(normalize(projectRoot), 'tsconfig.spec.json')
+          ];
+        }
+      }
+      workspace.projects[opts.name] = project;
+      return updateWorkspace(workspace);
+    }
+  ]);
+}
+
+function updateAppProjectx(opts: NormalizedOptions): Rule {
   return (tree: Tree) => {
     const workspace = getWorkspace(tree);
     let project = getProject(workspace, opts.name);
---
+++ packages/schematics/src/rules/updateJson.ts
@@ -0,0 +1,5 @@
+import { Rule, noop } from '@angular-devkit/schematics';
+
+export function updateJson(): Rule {
+  return noop;
+}
