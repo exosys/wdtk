2a62b608bcc38f59206f2ee53b8e50894cde3dbb
bogdant <bogdant@ganz.com>
2019-01-04 16:03:27 -0500

    Removed unused files

--- packages/schematics/src/util/string/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Upper camelCase to lowercase, hypenated
- */
-export function toFileName(s: string): string {
-  return s
-    .replace(/([a-z\d])([A-Z])/g, '$1_$2')
-    .toLowerCase()
-    .replace(/[ _]/g, '-');
-}
