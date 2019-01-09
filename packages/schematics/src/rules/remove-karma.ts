d1b0e1d4ab3fad621dc90050db8dea8b8e68e329
bogdant <bogdant@ganz.com>
2019-01-09 16:58:48 -0500

    Removed karma dependencies

--- packages/schematics/src/rules/remove-karma.ts
+++ packages/schematics/src/rules/remove-karma.ts
@@ -1,9 +1,46 @@
-import { Rule, Tree } from '@angular-devkit/schematics';
-import { join, normalize, Path } from '@angular-devkit/core';
+import { Rule, Tree, chain } from '@angular-devkit/schematics';
+import { join, normalize, DependencyNotFoundException } from '@angular-devkit/core';
 import * as ng from './../angular';
-import * as path from 'path';
+import { updateJsonFile } from './update-json-file';
 
 export function removeKarma<T>(projectName: string): Rule {
+  return (tree: Tree) => {
+    return chain([
+      (tree: Tree) => {
+        const project = ng.getProject(projectName, tree);
+        if (!project) {
+          return tree;
+        }
+        const projectRoot = normalize(project.root);
+        let projectSourceRoot = join(projectRoot, 'src');
+        if (project.sourceRoot) {
+          projectSourceRoot = normalize(project.sourceRoot);
+        }
+        tree.delete(join(projectRoot, 'karma.conf.js'));
+        tree.delete(join(projectRoot, 'tsconfig.spec.json'));
+        tree.delete(join(projectSourceRoot, 'test.ts'));
+        if (project.architect) {
+          const architect: any = project.architect;
+
+          if (architect.test) {
+            delete project.architect.test;
+          }
+          if (architect.lint) {
+            const pathToRemove = join(projectRoot, 'tsconfig.spec.json');
+            (<any>project.architect.lint).options.tsConfig = (<any>project.architect.lint).options.tsConfig.filter((path: any) => {
+              return path !== pathToRemove;
+            });
+          }
+        }
+
+        return ng.updateProject(projectName, project);
+      },
+      removeDependencies(projectName)
+    ]);
+  };
+}
+
+function updateProjectNgConf(projectName: string): Rule {
   return (tree: Tree) => {
     const project = ng.getProject(projectName, tree);
     if (!project) {
@@ -35,10 +72,23 @@ export function removeKarma<T>(projectName: string): Rule {
   };
 }
 
-function removeDependencies(): Rule {
-  return (tree: Tree) => {};
-}
-
-function updateProjectNgConf(): Rule {
-  return (tree: Tree) => {};
+function removeDependencies(projectName: string): Rule {
+  return (tree: Tree) => {
+    const project = ng.getProject(projectName, tree);
+    if (!project) {
+      return tree;
+    }
+    const projectRoot = normalize(project.root);
+    return updateJsonFile(`${projectRoot}/package.json`, (json: any) => {
+      let devDependencies = json.devDependencies;
+      Object.keys(devDependencies).forEach(function(key) {
+        if (key.includes('karma')) {
+          delete devDependencies[key];
+        }
+      });
+      json.devDependencies = {
+        ...devDependencies
+      };
+    });
+  };
 }
